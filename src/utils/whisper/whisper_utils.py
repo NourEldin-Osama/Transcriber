fcb46f380cc689cba62ec4ea84a704498549f209
NourEldin <noureldin.osama.saad@gmail.com>
2025-02-01 17:53:12 +0200

    feat: Implement WhisperRecognizer and utility functions for audio transcription

---
+++ src/utils/whisper/whisper_utils.py
@@ -0,0 +1,15 @@
+import faster_whisper
+import stable_whisper
+
+from ...config import settings
+from ...types.whisper.type_hints import WhisperModel
+
+
+def load_model() -> WhisperModel:  # type: ignore
+    if settings.whisper.use_faster_whisper:
+        return faster_whisper.WhisperModel(
+            settings.whisper.model_name_or_path,
+            compute_type=settings.whisper.ct2_compute_type,
+        )
+    else:
+        return stable_whisper.load_model(settings.whisper.model_name_or_path)
